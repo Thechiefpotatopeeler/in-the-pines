91e8e9e0d5bdd76b2f4dc6c817976498f90e7b23
Thechiefpotatopeeler <69015883+Thechiefpotatopeeler@users.noreply.github.com>
2024-02-24 19:40:01 +1300

    Added project to repo

---
+++ SceneCamera.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class SceneCamera : Camera3D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		//Position = GetParentNode3D().GetChild<CharacterBody3D>(0).Position;
+		//Rotation = GetParentNode3D().GetChild<CharacterBody3D>(0).Rotation;
+
+    }
+}
